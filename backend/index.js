06d7e6750e20ac004d53fcd0fb5ef39791e3b070
facunhn <facundo.nahon@gmail.com>
2025-12-11 12:10:02 -0300

    termine todo

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,46 @@
-import {subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic"
 import fs from "fs";
-startServer()
+import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 
-subscribeGETEvent("obras",obrasrespuesta)
+function Obras() {
+let obras = JSON.parse(fs.readFileSync("../data/obras.json", "utf-8"));
+return obras;
+}
+subscribeGETEvent("obras", Obras);
 
-function obrasrespuesta() {
+function g2(info) {
+let coleccion = JSON.parse(fs.readFileSync("../data/coleccion.json", "utf-8"));
 
-    let respuesta = fs.readFileSync("../data/obras.json", "utf-8");
-    console.log(respuesta)
-    return JSON.parse (respuesta);
+if (info.enColección === true) {
+coleccion.push(info.id);
+fs.writeFileSync("../data/coleccion.json", JSON.stringify(coleccion, null, 2));
+return true;
+}
+else {
+
+    coleccion = coleccion.filter(id =>  id !== info.id);  // => es igual a entonces Anotación: !== quiere decir si no es igual a...
+    
+    fs.writeFileSync("../data/coleccion.json", JSON.stringify(coleccion,null, 2));
+    
+    return false;
+    }
 }
 
+subscribePOSTEvent("modificarColección", g2);
+function coleccionids() {
+let coleccion = JSON.parse(fs.readFileSync("../data/coleccion.json", "utf-8"));
+return coleccion;
+}
+subscribeGETEvent("colección", coleccionids);
+
+function obrascoleccion() {
+let coleccion = JSON.parse(fs.readFileSync("../data/coleccion.json", "utf-8"));
+let obras = JSON.parse(fs.readFileSync("../data/obras.json", "utf-8"));
+
+let filtradas = obras.filter(function (obra) {
+return coleccion.includes(obra.id);
+});
+
+return filtradas;
+}
+subscribeGETEvent("obrasColección", obrascoleccion);
+startServer();
